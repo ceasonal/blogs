187ff9e0be777eddd63bb934b299958b65db00f9
ceabenshawn <ceasonalgamer@gmail.com>
2023-10-02 13:12:43 +0530

    updated 🔥

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ['Home', 'AboutUs', 'Contact','kaust'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
@@ -55,7 +55,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+           MetaBlog
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -111,7 +111,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+           MetaBlog
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -110,7 +110,7 @@ const SignIn = () => {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="s" variant="body2">
+                  <Link href="/signup" variant="body2">
                     "Don't have an account? Sign Up"
                   </Link>
                 </Grid>
