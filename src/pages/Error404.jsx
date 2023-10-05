27de524ec087af6071e95fe527f954c3a1384fd4
ceabenshawn <ceasonalgamer@gmail.com>
2023-10-05 23:29:16 +0530

    fix 🐞

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,18 +1,30 @@
-const Error404 = () => {
-    const backgroundStyle = {
-        backgroundImage: `url('https://w0.peakpx.com/wallpaper/104/618/HD-wallpaper-anime-error-female-dress-black-cute-hair-windows-girl-anime-page.jpg')`,
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      };
-  return (
-    <div style={backgroundStyle}>
-    </div>
-  )
-}
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Navbar from "../components/Navbar";
 
-export default Error404
+const Error404 = () => {
+  const backgroundStyle = {
+    backgroundImage: `url('https://w0.peakpx.com/wallpaper/104/618/HD-wallpaper-anime-error-female-dress-black-cute-hair-windows-girl-anime-page.jpg')`,
+    width: "100vw",
+    height: "100vh",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <div style={backgroundStyle}>
+          <Navbar />
+        </div>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default Error404;
