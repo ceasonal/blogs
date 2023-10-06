80ab86d843320a26f6b41440c7c5989daea2574e
ceabenshawn <ceasonalgamer@gmail.com>
2023-10-06 22:41:49 +0530

    contact page pog

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,207 @@
-const Contact = () => {
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";  
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Navbar from "../components/Navbar";
+
+function Copyright(props) {
   return (
-    <div>Contact</div>
-  )
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="/home">
+        MetaBlog
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
-export default Contact
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
+      email: data.get("email"),
+      message: data.get("message")
+    });
+
+    const firstName = data.get("firstName");
+    const lastName = data.get("lastName");
+    const email = data.get("email");
+    const message = data.get("message");
+
+    if (!firstName || !lastName || !email || !message) {
+      alert("Please fill in all the fields");
+      return;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailRegex.test(email)) {
+      alert("Invalid email format");
+      return;
+    }
+
+    alert("Message sent successfully!");
+
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <Navbar />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={5}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={7} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Get in Touch
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              sx={{ mt: 3 }}
+              onSubmit={handleSubmit}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    inputProps={{
+                      style: {
+                        color: "white",
+                        borderBottom: "1px solid white",
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        color: "white",
+                      },
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                    inputProps={{
+                      style: {
+                        color: "white",
+                        borderBottom: "1px solid white",
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        color: "white",
+                      },
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    inputProps={{
+                      style: {
+                        color: "white",
+                        borderBottom: "1px solid white",
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        color: "white",
+                      },
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="message"
+                    label="Message"
+                    name="message"
+                    multiline 
+                    rows={4} 
+                    inputProps={{
+                      style: {
+                        color: "white",
+                        borderBottom: "1px solid white",
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        color: "white",
+                      },
+                    }}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Submit
+              </Button>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
